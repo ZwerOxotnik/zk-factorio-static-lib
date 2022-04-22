21fc3e04a20d35232d02728c5eb98fbd13278b6e
ZwerOxotnik <zweroxotnik@gmail.com>
2022-04-22 09:34:56 +0300

    Improve locale.lua

--- lualibs/locale.lua
+++ lualibs/locale.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 
-local ceil = math.ceil
+local type = type
 local deepcopy = table.deepcopy
 local tinsert = table.insert
 
@@ -9,30 +9,44 @@ local tinsert = table.insert
 ---@param array table
 ---@return table?
 M.array_to_locale = function(array)
-	local final_data = {}
+	local final_data
 	if #array >= 380 then
-			log("Too much data")
-			return
-	elseif #array > 10 then
-		for i=1, ceil(#array/20)+1 do
-			final_data[i] = {""}
-		end
+		log("Too much data")
+		return
+	elseif #array <= 10 then
+		final_data = array
+	else
+		final_data = {{''}}
 		local i = 1
 		local j = 2
 		for _, _data in pairs(array) do
-			final_data[i][j] = _data
+			local row = final_data[i]
+			if type(_data) == "string" then
+				if j > 2 then
+					local prev_data = row[j-1]
+					if type(prev_data) == "string" then
+						row[j-1] = prev_data .. _data
+						goto skip_itertion
+					end
+				end
+			end
+			row[j] = _data
 			if j >= 20 then
 				i = i + 1
+				final_data[i] = {''}
 				j = 2
 			else
 				j = j + 1
 			end
+			::skip_itertion::
 		end
-	else
-		final_data = array
 	end
 
-	if type(final_data[1]) ~= "string" then
+	if type(final_data[1]) == "string" then
+		if final_data[1] ~= '' then
+			tinsert(final_data, 1, '')
+		end
+	else
 		tinsert(final_data, 1, '')
 	end
 	if #final_data > 20 then
@@ -47,66 +61,87 @@ end
 ---@param array table
 ---@return table?
 M.array_to_locale_as_new = function(array)
-	local final_data = {}
-	if #array >= 380 then
-			log("Too much data")
-			return
-	elseif #array > 10 then
-		for i=1, ceil(#array/20)+1 do
-			final_data[i] = {""}
-		end
-		local i = 1
-		local j = 2
-		for _, _data in pairs(array) do
-			final_data[i][j] = deepcopy(_data)
-			if j >= 20 then
-				i = i + 1
-				j = 2
+	local locale = M.array_to_locale(array)
+	if locale then
+		return deepcopy(locale)
+	end
+end
+
+
+---@param locale table
+---@return table
+M.locale_to_array = function(locale)
+	local v1 = locale[1]
+	if not (type(v1) == "string" and #v1 == 0) then return locale end
+
+	local array = {}
+	for _, _data in pairs(locale) do
+		local _type = type(_data)
+		if _type == "table" then
+			local new_array = M.locale_to_array(_data)
+			if new_array == _data then
+				array[#array+1] = _data
 			else
-				j = j + 1
+				for _, __data in next, new_array do
+					array[#array+1] = __data
+				end
 			end
+		elseif _type == "string" then
+			if #_data ~= 0 then
+				array[#array+1] = _data
+			end
+		else
+			array[#array+1] = _data
 		end
-	else
-		final_data = array
 	end
-
-	if type(final_data[1]) ~= "string" then
-		tinsert(final_data, 1, '')
-	end
-	if #final_data > 20 then
-		log("Too much data")
-		return
-	end
-
-	return final_data
+	return array
 end
 
 
 ---@vararg table
 ---@return table
 M.merge_locales = function(...)
-  local args = {...}
-  local new_locale = {''}
+	local args = {...}
+	local new_locale = {}
 
-  for i=1, #args do
-    new_locale[i+1] = args[i]
-  end
+	for _, locale in pairs(args) do
+		local _, v = next(locale)
+		if type(v) == "table" then
+			new_locale[#new_locale+1] = locale
+		else
+			for _, data in pairs(locale) do
+				local _type = type(data)
+				if _type == "table" then
+					local new_array = M.locale_to_array(data)
+					if new_array == data or #new_array == 0 then
+						new_locale[#new_locale+1] = data
+					else
+						for i=1, #new_array do
+							new_locale[#new_locale+1] = new_array[i]
+						end
+					end
+				elseif _type == "string" then
+					if #data ~= 0 then
+						new_locale[#new_locale+1] = data
+					end
+				else
+					new_locale[#new_locale+1] = data
+				end
+			end
+		end
+	end
 
-  return new_locale
+	return M.array_to_locale(new_locale)
 end
 
 
 ---@vararg table
 ---@return table
 M.merge_locales_as_new = function(...)
-  local args = {...}
-  local new_locale = {''}
-
-  for i=1, #args do
-    new_locale[i+1] = deepcopy(args[i])
-  end
-
-  return new_locale
+	return deepcopy(M.merge_locales(...))
 end
+
+local t2 = M.merge_locales({'', nil, "tsef", {"test", 3, 2, 4}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}},{'', nil, "tsef", {"test"}, {'', "test", {'', "test"}}}, nil, {{"fesfes"}, nil, nil, 44})
+g = 4
 
 return M
