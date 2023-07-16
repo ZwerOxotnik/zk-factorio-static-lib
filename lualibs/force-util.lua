192472f824ab34f88e21ade9c4af02eb30e34e41
ZwerOxotnik <zweroxotnik@gmail.com>
2023-07-17 00:34:07 +0300

    Add force_util.count_techs

--- lualibs/force-util.lua
+++ lualibs/force-util.lua
@@ -31,4 +31,24 @@ force_util.research_techs_safely = function(force, techs)
 end
 
 
+---@param force LuaForce
+---@return integer, integer, number # researched_techs, total_techs, tech_ratio
+force_util.count_techs = function(force, techs)
+	local researched_techs = 0
+	local total_techs = 0
+
+	local technologies = force.technologies
+	for _, tech in pairs(technologies) do
+		if tech.research_unit_count_formula == nil and not tech.upgrade then
+			total_techs = total_techs + 1
+			if tech.researched then
+				researched_techs = researched_techs + 1
+			end
+		end
+	end
+
+	return researched_techs, total_techs, researched_techs / total_techs
+end
+
+
 return force_util
