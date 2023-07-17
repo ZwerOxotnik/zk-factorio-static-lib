76c7066ef458a6d134bc4aacc0ccb7ed4e914176
ZwerOxotnik <zweroxotnik@gmail.com>
2023-07-17 15:38:52 +0300

    Fix all_*_utils files

--- all_control_utils.lua
+++ all_control_utils.lua
@@ -3,13 +3,16 @@ local zo_utils = {}
 
 
 do
-	local util_pathes = {
+	local util_paths = {
 		"number-util", "locale", "time-util", "lauxlib", "coordinates-util",
 		"control_stage/force-util", "control_stage/entity-util",
 		"control_stage/player-util", "control_stage/market-util",
 		"control_stage/inventory-util", "control_stage/surface-util"
 	}
-	for _, path in ipairs(util_pathes) do
+	for i, path in ipairs(util_paths) do
+		util_paths[i] = "lualibs/" .. path
+	end
+	for _, path in ipairs(util_paths) do
 		for k, v in pairs(require(path)) do
 			zo_utils[k] = v
 		end
--- all_data_utils.lua
+++ all_data_utils.lua
@@ -3,10 +3,13 @@ local zo_utils = {}
 
 
 do
-	local util_pathes = {
+	local util_paths = {
 		"locale", "time-util", "lauxlib", "number-util", "coordinates-util"
 	}
-	for _, path in ipairs(util_pathes) do
+	for i, path in ipairs(util_paths) do
+		util_paths[i] = "lualibs/" .. path
+	end
+	for _, path in ipairs(util_paths) do
 		for k, v in pairs(require(path)) do
 			zo_utils[k] = v
 		end
