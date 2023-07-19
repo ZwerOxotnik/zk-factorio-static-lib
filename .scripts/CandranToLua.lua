2b3ba663c5af3c68dbc468e43f57f028b60713a7
ZwerOxotnik <zweroxotnik@gmail.com>
2023-07-19 16:45:02 +0300

    Improve

---
+++ .scripts/CandranToLua.lua
@@ -0,0 +1,47 @@
+local candran = require("candran") -- https://github.com/Reuh/candran
+local re = require("re") -- http://www.inf.puc-rio.br/~roberto/lpeg/re.html
+
+local options = {
+	target = "lua52", -- compiler target. "lua54", "lua53", "lua52", "luajit" or "lua51" (default is automatically selected based on the Lua version used).
+	indentation = "\t", -- character(s) used for indentation in the compiled file.
+	newline = "\n", -- character(s) used for newlines in the compiled file.
+	variablePrefix = "__CAN_", -- Prefix used when Candran needs to set a local variable to provide some functionality (example: to load LuaJIT's bit lib when using bitwise operators).
+	mapLines = false, -- if true, compiled files will contain comments at the end of each line indicating the associated line and source file. Needed for error rewriting.
+	chunkname = "models/free-market.can", -- the chunkname used when running code using the helper functions and writing the line origin comments. Candran will try to set it to the original filename if it knows it.
+	rewriteErrors = false, -- true to enable error rewriting when loading code using the helper functions. Will wrap the whole code in a xpcall().
+	builtInMacros = true, -- false to disable built-in macros __*__
+	preprocessorEnv = {}, -- environment to merge with the preprocessor environement
+	import = {} -- list of modules to automatically import in compiled files (using #import("module",{loadLocal=false}))
+}
+-- Looks for "--#" except "--#region" and "--#endregion"
+local PATTERN = "'--#'!(('region')/('endregion'))"
+
+
+---@param source string
+---@param destination string
+local function candran_to_lua(source, destination)
+	-- Reads text
+	local f = io.open(source)
+	local contents = f:read("*a")
+	f:close()
+
+	-- A dirty workaround for compatibility with vscode
+	contents = re.gsub(contents, PATTERN, '#')
+
+	-- Runs preprocess
+	print("Compiling " .. source .. " in " .. destination)
+	local compiled, err = candran.preprocess(contents, options)
+	if not compiled then
+		print(err)
+		return
+	end
+
+	-- Writes code
+	f = io.open(destination, "w+")
+	f:write(compiled)
+	f:close()
+end
+
+
+candran_to_lua("./all_data_utils.can", "./all_data_utils.lua")
+candran_to_lua("./all_control_utils.can", "./all_control_utils.lua")
--- all_data_utils.lua
+++ all_data_utils.lua
@@ -3,18 +3,26 @@ local zo_utils = {}
 
 
 do
-	local util_paths = {
-		"locale", "time-util", "lauxlib", "number-util", "coordinates-util"
-	}
-	for i, path in ipairs(util_paths) do
-		util_paths[i] = "lualibs/" .. path
-	end
-	for _, path in ipairs(util_paths) do
-		for k, v in pairs(require(path)) do
-			zo_utils[k] = v
-		end
-	end
+	local util
+	util = require("lualibs/locale")
+	zo_utils.array_to_locale = util.array_to_locale
+	zo_utils.merge_locales = util.merge_locales
+	zo_utils.merge_locales_as_new = util.merge_locales_as_new
+	zo_utils.locale_to_array = util.locale_to_array
+	zo_utils.array_to_locale_as_new = util.array_to_locale_as_new
+	util = require("lualibs/time-util")
+	zo_utils.ticks_to_game_mm_ss = util.ticks_to_game_mm_ss
+	util = require("lualibs/lauxlib")
+	zo_utils.count_levels = util.count_levels
+	zo_utils.get_first_lua_func_info = util.get_first_lua_func_info
+	util = require("lualibs/number-util")
+	zo_utils.format_number = util.format_number
+	util = require("lualibs/coordinates-util")
+	zo_utils.random_position_in_radius = util.random_position_in_radius
+	zo_utils.get_distance = util.get_distance
 end
+zo_utils.build = 5
 
 
 return zo_utils
+
