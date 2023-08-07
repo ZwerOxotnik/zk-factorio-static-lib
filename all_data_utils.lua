763c5dcaedce93b92eea653bddcdef38b86415e7
ZwerOxotnik <zweroxotnik@gmail.com>
2023-08-07 16:14:46 +0300

    Add biter-util.lua

--- all_data_utils.lua
+++ all_data_utils.lua
@@ -6,11 +6,11 @@ do
 	local util
 	util = require("lualibs/locale")
 	zo_utils.builds["locale"] = 1
+	zo_utils.merge_locales = util.merge_locales
+	zo_utils.array_to_locale = util.array_to_locale
 	zo_utils.merge_locales_as_new = util.merge_locales_as_new
 	zo_utils.array_to_locale_as_new = util.array_to_locale_as_new
-	zo_utils.merge_locales = util.merge_locales
 	zo_utils.locale_to_array = util.locale_to_array
-	zo_utils.array_to_locale = util.array_to_locale
 	util = require("lualibs/time-util")
 	zo_utils.builds["time-util"] = 1
 	zo_utils.ticks_to_game_mm_ss = util.ticks_to_game_mm_ss
@@ -23,8 +23,8 @@ do
 	zo_utils.format_number = util.format_number
 	util = require("lualibs/coordinates-util")
 	zo_utils.builds["coordinates-util"] = 1
-	zo_utils.get_distance = util.get_distance
 	zo_utils.random_position_in_radius = util.random_position_in_radius
+	zo_utils.get_distance = util.get_distance
 end
 zo_utils.build = 5
 
