c94adbcb8e2e5432c87928563e0aeb3833b8438b
ZwerOxotnik <zweroxotnik@gmail.com>
2023-08-08 16:25:44 +0300

    Improve prototype_util

--- lualibs/control_stage/prototype-util.lua
+++ lualibs/control_stage/prototype-util.lua
@@ -1,17 +1,19 @@
 ---@class ZOprototype_util
-local prototype_util = {build = 3}
+local prototype_util = {build = 4}
 
 
 --[[
 prototype_util.get_first_valid_prototype(prototypes, names): string?
-prototype_util.get_recipes_by_product(Product|Ingredient): LuaCustomTable<string, LuaRecipePrototype>
-prototype_util.get_recipes_by_ingredient(Product|Ingredient): LuaCustomTable<string, LuaRecipePrototype>
+prototype_util.get_recipes_by_product(Product|Ingredient|table): LuaCustomTable<string, LuaRecipePrototype>
+prototype_util.get_recipes_by_ingredient(Product|Ingredient|table): LuaCustomTable<string, LuaRecipePrototype>
 prototype_util.get_recipes_by_item_ingredient(item_name): LuaCustomTable<string, LuaRecipePrototype>
 prototype_util.get_recipes_by_item_product(item_name): LuaCustomTable<string, LuaRecipePrototype>
 prototype_util.get_recipes_by_fluid_ingredient(ingredient_name): LuaCustomTable<string, LuaRecipePrototype>
 prototype_util.get_recipes_by_fluid_product(ingredient_name): LuaCustomTable<string, LuaRecipePrototype>
-prototype_util.get_recipes_by_ingredient_and_its_successors(Product|Ingredient, depth=1): LuaRecipePrototype[]
-prototype_util.get_recipes_by_product_and_its_predecessors(Product|Ingredient,  depth=1): LuaRecipePrototype[]
+prototype_util.get_recipes_successors(  LuaRecipe|LuaRecipePrototype|table, depth=1): LuaRecipePrototype[]
+prototype_util.get_recipes_predecessors(LuaRecipe|LuaRecipePrototype|table, depth=1): LuaRecipePrototype[]
+prototype_util.get_recipes_and_its_successors_by_ingredient(Product|Ingredient|table, depth=1): LuaRecipePrototype[]
+prototype_util.get_recipes_and_its_predecessors_by_product( Product|Ingredient|table, depth=1): LuaRecipePrototype[]
 prototype_util.get_result_recipes_by_entity_name(entity_name): LuaCustomTable<string, LuaRecipePrototype>
 ]]
 
@@ -94,10 +96,104 @@ function prototype_util.get_recipes_by_ingredient(ingredient)
 end
 
 
----@param ingredient Product|Ingredient|table
----@param depth integer # 1 by default
+---@param recipe LuaRecipe|LuaRecipePrototype|table
+---@param depth integer? # 1 by default
 ---@return LuaRecipePrototype[]
-function prototype_util.get_recipes_by_ingredient_and_its_successors(ingredient, depth)
+function prototype_util.get_recipes_successors(recipe, depth)
+	depth = depth or 1
+
+	local result_recipes = {nil}
+	local products = recipe.products
+	for i = 1, #products do
+		recipes = prototype_util.get_recipes_by_ingredient(products[i])
+		for _, _recipe in pairs(recipes) do
+			if _recipe ~= recipe then
+				for j = 1, #result_recipes do
+					if _recipe == result_recipes[j] then
+						goto continuei2
+					end
+				end
+				result_recipes[#result_recipes+1] = _recipe
+				:: continuei2 ::
+			end
+		end
+	end
+
+	for _ = 1, depth-1 do
+		for _, _recipe in pairs(recipes) do
+			if _recipe ~= recipe then
+				local _products = _recipe.products
+				for j = 1, #_products do
+					recipes = prototype_util.get_recipes_by_ingredient(_products[j])
+					for _, __recipe in pairs(recipes) do
+						for j2 = 1, #result_recipes do
+							if __recipe == result_recipes[j2] then
+								goto continuei2
+							end
+						end
+						result_recipes[#result_recipes+1] = __recipe
+						:: continuei2 ::
+					end
+				end
+			end
+		end
+	end
+
+	return result_recipes
+end
+
+
+---@param recipe LuaRecipe|LuaRecipePrototype|table
+---@param depth integer? # 1 by default
+---@return LuaRecipePrototype[]
+function prototype_util.get_recipes_predecessors(recipe, depth)
+	depth = depth or 1
+
+	local result_recipes = {nil}
+	local ingredients = recipe.ingredients
+	for i = 1, #ingredients do
+		recipes = prototype_util.get_recipes_by_product(ingredients[i])
+		for _, _recipe in pairs(recipes) do
+			if _recipe ~= recipe then
+				for j = 1, #result_recipes do
+					if _recipe == result_recipes[j] then
+						goto continuei2
+					end
+				end
+				result_recipes[#result_recipes+1] = _recipe
+				:: continuei2 ::
+			end
+		end
+	end
+
+	for _ = 1, depth-1 do
+		for _, _recipe in pairs(recipes) do
+			if _recipe ~= recipe then
+				local _ingredients = _recipe.ingredients
+				for j = 1, #_ingredients do
+					recipes = prototype_util.get_recipes_by_product(_ingredients[j])
+					for _, __recipe in pairs(recipes) do
+						for j2 = 1, #result_recipes do
+							if __recipe == result_recipes[j2] then
+								goto continuei2
+							end
+						end
+						result_recipes[#result_recipes+1] = __recipe
+						:: continuei2 ::
+					end
+				end
+			end
+		end
+	end
+
+	return result_recipes
+end
+
+
+---@param ingredient Product|Ingredient|table
+---@param depth integer? # 1 by default
+---@return LuaRecipePrototype[]
+function prototype_util.get_recipes_and_its_successors_by_ingredient(ingredient, depth)
 	depth = depth or 1
 
 	local recipes = prototype_util.get_recipes_by_ingredient(ingredient)
@@ -113,7 +209,7 @@ function prototype_util.get_recipes_by_ingredient_and_its_successors(ingredient,
 				recipes = prototype_util.get_recipes_by_ingredient(products[j])
 				for _, _recipe in pairs(recipes) do
 					for j2 = 1, #result_recipes do
-						if _recipe == result_recipes[j2] then -- TODO: test
+						if _recipe == result_recipes[j2] then
 							goto continuei2
 						end
 					end
@@ -129,9 +225,9 @@ end
 
 
 ---@param product Product|Ingredient|table
----@param depth integer # 1 by default
+---@param depth integer? # 1 by default
 ---@return LuaRecipePrototype[]
-function prototype_util.get_recipes_by_product_and_its_predecessors(product, depth)
+function prototype_util.get_recipes_and_its_predecessors_by_product(product, depth)
 	depth = depth or 1
 
 	local recipes = prototype_util.get_recipes_by_product(product)
@@ -147,7 +243,7 @@ function prototype_util.get_recipes_by_product_and_its_predecessors(product, dep
 				recipes = prototype_util.get_recipes_by_product(ingredients[j])
 				for _, _recipe in pairs(recipes) do
 					for j2 = 1, #result_recipes do
-						if _recipe == result_recipes[j2] then -- TODO: test
+						if _recipe == result_recipes[j2] then
 							goto continuei2
 						end
 					end
