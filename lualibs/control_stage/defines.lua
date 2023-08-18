3cf85e1b5287fcc448dc33c7eacedc1d94545836
ZwerOxotnik <zweroxotnik@gmail.com>
2023-08-18 22:30:33 +0300

    Add defines.lua for control stage

---
+++ lualibs/control_stage/defines.lua
@@ -0,0 +1,12 @@
+---@class ZK_defines
+local ZK_defines = {}
+
+
+---@type string[]
+ZK_defines.turret_types = {
+	"artillery-turret", "electric-turret", "fluid-turret",
+	"ammo-turret", "turret"
+}
+
+
+return ZK_defines
